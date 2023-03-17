5e3be8f69dc2f93ad19857a5574e07422f60a64b
Kojoel <nanaabruquah1s@gmail.com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exit:qclear>
2023-03-17 01:27:52 +0000

    Task8Unix is simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A c program that prints a line to teh standard e
+ * Return 1 (Success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpand that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+return (1);
