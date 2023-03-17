39accf8f4e459853a0387e7c1c5eada5f9f68cf7
Kojoel <nanaabruquah1s@gmail.com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exit:qclear>
2023-03-17 23:23:44 +0000

    Print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Description: Print alphabets lowercase
+  *Return: Always 0
+  */
+int main(void)
+{
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+return (0);
+}
