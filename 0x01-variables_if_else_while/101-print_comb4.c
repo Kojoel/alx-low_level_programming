a110ffe444f523722a1c9bf457b8f6e94cbfc264
Kojoel <nanaabruquah1s@gmail.com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exit:qclear>
2023-03-18 01:13:25 +0000

    Print all possible comibinations of 3 digit numbers)

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Description: Print all possible combinations of three digit numbers
+  *Return: always 0
+  */
+int main(void)
+{
+	int n, m, o;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (o = 50; o < 58; o++)
+			{
+				putchar(n);
+				putchar(m);
+				putchar(o);
+				if (n != 55 || m != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+return (0);
+}
