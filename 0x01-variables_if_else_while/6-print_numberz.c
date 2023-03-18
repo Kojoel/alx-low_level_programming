9045d2d70f67e8345abc85d2322dc9bbdc615c37
Kojoel <nanaabruquah1s@gmail.com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exit:qclear>
2023-03-18 00:19:03 +0000

    Print out single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Description: Print all single digit numbers fo base 10 starting from 0
+  *Return: always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 0; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+return (0);
+}
