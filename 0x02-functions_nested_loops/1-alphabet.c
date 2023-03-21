e60add3bed64574b8e9a87477e71edda5b5907b1
Kojoel <nanaabruquah1s@gmail.com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exit:qclear>
2023-03-21 23:18:04 +0000

    print the alphabet in in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *main - Entry point
+  *_putchar: print the alphabets putchar
+  * Description: print the alphabets putchar
+  * Return: always 0
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+return (0);
+}
